c13132d42e6f7596cdc0bd4f3838b4d4e5c4d852
Djessy Coiffe <djessy.coiffe@gmail.com>
2025-01-28 16:00:18 +0100

    wip

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const { $supabase } = useNuxtApp();
-
-  const { data: { user }, error } = await $supabase.auth.getUser();
-
-  if (!user) {
-    return navigateTo('/login');
-  }
-});
